c551978e2f999b63f71126825311fef62adf55ac
Oleg Smushkevich <olegs777@bezeqint.net>
2020-10-11 13:26:33 +0300

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import logo from "../../img/logo-white.png";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <header className="header">Some text..</header>
+        <header className="header">
+          <div className="logo-box">
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
+        </header>
       </div>
     );
   }
