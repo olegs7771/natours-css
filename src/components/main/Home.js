440ad6aacfc7866d333eb8972e0e1664c2ef3801
olegs7771 <olegs7771@gmail.com>
2020-10-11 22:49:05 +0300

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -9,8 +9,16 @@ class Home extends Component {
           <div className="logo-box">
             <img src={logo} alt="Logo" className="logo" />
           </div>
+
+          <div className="text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary-main">Outdoors</span>
+            <span className="heading-primary-sub">is where life happens</span>
+          </h1>
+
+          </div>
         </header>
-      </div>
+     </div>
     );
   }
 }
