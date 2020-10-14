ddb474880f4fa89e04ff5ff5e73150a3387221f0
Oleg Smushkevich <olegs777@bezeqint.net>
2020-10-14 08:57:53 +0300

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -6,16 +6,18 @@ class Home extends Component {
     return (
       <div>
         <header className="header">
-          <div className="logo-box">
-            <img src={logo} alt="Logo" className="logo" />
+          <div className="header__logo-box">
+            <img src={logo} alt="Logo" className="header__logo" />
           </div>
 
-          <div className="text-box">
+          <div className="header__text-box">
             <h1 className="heading-primary">
-              <span className="heading-primary-main">Outdoors</span>
-              <span className="heading-primary-sub">is where life happens</span>
+              <span className="heading-primary--main">Outdoors</span>
+              <span className="heading-primary--sub">
+                is where life happens
+              </span>
             </h1>
-            <a href="#" className="btn btn-white btn-animated">
+            <a href="#" className="btn btn--white btn--animated">
               Discover Our Tours
             </a>
           </div>
