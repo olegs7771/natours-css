75765731ca7cd7989291e74bc3269c4942e3cc02
olegs7771 <olegs7771@gmail.com>
2020-10-14 23:53:51 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //Components
 import Home from "../src/components/main/Home";
 
-import "./App.css";
+import "./sass/main.scss";
 
 const App = () => {
   return (
