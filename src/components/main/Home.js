c0bd993ec9ddb64dabecc8feafa3f5e45efa34c5
Oleg Smushkevich <olegs777@bezeqint.net>
2020-10-17 13:33:37 +0300

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -20,9 +20,41 @@ class Home extends Component {
             <a href="#" className="btn btn--white btn--animated">
               Discover Our Tours
             </a>
-            
           </div>
         </header>
+        <main>
+          <section className="section-about">
+            <div className="u-center-text u-margin-bottom-big">
+              <h2 className="heading-secondary">
+                Exiting tours for adventurous people
+              </h2>
+            </div>
+
+            <div className="row">
+              <div className="col-1-of-2 ">
+                <h3 className="heading-tertiary u-margin-bottom-small">
+                  You're going to fall in love with nature
+                </h3>
+                <p className="paragraph">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id
+                  dolorum voluptatum eligendi corrupti, corporis harum culpa
+                  possimus! Vel, distinctio nesciunt!
+                </p>
+                <h3 className="heading-tertiary u-margin-bottom-small">
+                  Live advantures like you never lived before
+                </h3>
+                <p className="paragraph">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Beatae, eos alias unde quod, quam repellat quisquam quis
+                  obcaecati illum non ea quo mollitia molestiae. Quaerat quae
+                  impedit distinctio velit ab.
+                </p>
+                <a href="#" className="btn-text"></a>
+              </div>
+              <div className="col-1-of-2 ">Image Composition</div>
+            </div>
+          </section>
+        </main>
 
         {/* <section className="grid-test">
           <div className="row">
@@ -43,7 +75,6 @@ class Home extends Component {
             <div className="col-1-of-4">Col 1 of 4</div>
             <div className="col-1-of-4">Col 1 of 4</div>
             <div className="col-1-of-4">Col 1 of 4</div>
-            
           </div>
           <div className="row">
             <div className="col-1-of-4">Col 1 of 4</div>
