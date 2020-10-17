f8c6e2f6c451d59e07e28b3e0d827398c70c41da
olegs7771 <olegs7771@gmail.com>
2020-10-17 15:53:03 +0300

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import logo from "../../img/logo-white.png";
+import nat1large from '../../img/nat-1-large.jpg';
 
 class Home extends Component {
   render() {
@@ -45,14 +46,20 @@ class Home extends Component {
                 </h3>
                 <p className="paragraph">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Beatae, eos alias unde quod, quam repellat quisquam quis
-                  obcaecati illum non ea quo mollitia molestiae. Quaerat quae
-                  impedit distinctio velit ab.
+                  Beatae, eos alias unde quod, quam repellat quisquam
                 </p>
-                <a href="#" className="btn-text"></a>
+                <a href="#" className="btn-text">Learn more &rarr;</a>
               </div>
-              <div className="col-1-of-2 ">Image Composition</div>
+              <div className="col-1-of-2 ">
+
+                <div className="composition">
+                  <img src={nat1large} alt="" className="composition__photo composition_photo-p1"/>
+                  <img src="" alt="" className="composition__photo composition_photo-p1"/>
+                  <img src="" alt="" className="composition__photo composition_photo-p1"/>
+                </div>
+              </div>
             </div>
+            
           </section>
         </main>
 
