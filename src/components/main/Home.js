989b16e6c72dc8da2951b994628e2575fb8895b4
olegs7771 <olegs7771@gmail.com>
2020-10-20 15:33:36 +0300

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -136,10 +136,10 @@ class Home extends Component {
               <div className="col-1-of-3">
                 <div className="card">
                   <div className="card__side card__side--front">Front</div>
-                </div>
-                <div className="card">
                   <div className="card__side card__side--back">Back</div>
                 </div>
+              
+              
               </div>
               <div className="col-1-of-3">Col 1 of 3</div>
               <div className="col-1-of-3">Col 1 of 3</div>
