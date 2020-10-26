7c14e91d5f735737c2890b9c1be9c4ec2b66a7da
olegs7771 <olegs7771@gmail.com>
2020-10-26 14:36:54 +0200

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -358,6 +358,34 @@ class Home extends Component {
                       Email
                     </label>
                   </div>
+                  {/* Radio Buttons */}
+                  <div className="form__group">
+                    <div className="form__radio-group">
+                      <input
+                        type="radio"
+                        className="form__radio-input"
+                        id="small"
+                        name="size"
+                      />
+                      <label htmlFor="small" className="form__radio-label">
+                        {/* We cant style original radio buttons so we create own */}
+                        <span className="form__radio-button"></span>
+                        Small tour group
+                      </label>
+                    </div>
+                    <div className="form__radio-group">
+                      <input
+                        type="radio"
+                        className="form__radio-input"
+                        id="large"
+                        name="size"
+                      />
+                      <label htmlFor="large" className="form__radio-label">
+                        <span className="form__radio-button"></span>
+                        Large tour group
+                      </label>
+                    </div>
+                  </div>
                 </form>
               </div>
             </div>
